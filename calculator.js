5da93583a5a82074163d4aee19e0840de091bca3
leandrocruz <leandro.dinamo@gmail.com>
2022-05-07 04:34:50 -0300

    CSS tweaks, new Javascript functions, DOM handling
    HTML:
      *Add a second display row to show the pressed button value.
    
    CSS:
      *New calculator shadow.
      *New display shadow.
      *New calculator color.
      *New page background color.
    
    Javascript:
      *Add DOM manipulation to wire the buttons and the screen to the code.
      *New function (input). To display the pressed numbers on the calculator
      display, store the value after an operator is pressed and apply the
      function operate when the equal (=) button is pressed. It's needs some
      adjustment to work properly.
      *Rework the basics operations functions to work with the array method
      reduce.

--- calculator.js
+++ calculator.js
@@ -1,29 +1,135 @@
-function add(x, y) {
-    return x + y;
-};
+const display = document.querySelector('#display')
+const display2 = document.querySelector('#display2')
+let displayContent = document.createElement('p');
+let displayContent2 = document.createElement('p');
+displayContent.textContent = ''
+displayContent2.textContent = ''
+display.append(displayContent)
+display2.append(displayContent2)
 
-function subtract(x, y) {
-    return x - y;
-};
+function input() {
+    let show = ''
+    let numbers = []
+    let op = ''
+    const buttons = document.querySelectorAll('.btn');
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            if (button.id == 1){
+                show += 1
+                displayContent2.textContent = show
+            } else if (button.id == 2){
+                show += 2
+                displayContent2.textContent = show
+            } else if (button.id == 3){
+                show += 3
+                displayContent2.textContent = show
+            } else if (button.id == 4){
+                show += 4
+                displayContent2.textContent = show
+            } else if (button.id == 5){
+                show += 5
+                displayContent2.textContent = show
+            } else if (button.id == 6){
+                show += 6
+                displayContent2.textContent = show
+            } else if (button.id == 7){
+                show += 7
+                displayContent2.textContent = show
+            } else if (button.id == 8){
+                show += 8
+                displayContent2.textContent = show
+            } else if (button.id == 9){
+                show += 9
+                displayContent2.textContent = show
+            } else if (button.id == 0){
+                show += 0
+                displayContent2.textContent = show
+            } else if (button.id == '+'){
+                if (numbers.length == 2) {
+                    numbers.push(show)
+                    displayContent.textContent = numbers[0] + op + numbers[1]
+                    let result = operate(numbers, op)
+                    displayContent2.textContent = result
+                    show = ''
+                    numbers.length = 0
+                    numbers.push(result)
+                }
+                numbers.push(show)
+                op = '+'
+                show = ''
+                displayContent.textContent = numbers[0] + op
+                displayContent2.textContent = show
+            } else if (button.id == '-'){
+                numbers.push(show)
+                op = '-'
+                show = ''
+                displayContent.textContent = numbers[0] + op
+                displayContent2.textContent = show
+            } else if (button.id == '*'){
+                numbers.push(show)
+                op = '*'
+                show = ''
+                displayContent.textContent = numbers[0] + op
+                displayContent2.textContent = show
+            } else if (button.id == '/'){
+                numbers.push(show)
+                op = '÷'
+                show = ''
+                displayContent.textContent = numbers[0] + op
+                displayContent2.textContent = show
+            } else if (button.id == '='){
+                numbers.push(show)
+                displayContent.textContent = numbers[0] + op + numbers[1]
+                let result = operate(numbers, op)
+                displayContent2.textContent = result
+                show = ''
+                numbers.length = 0
+                numbers.push(result)
+            } else if (button.id == 'c'){
+                window.location.reload()
+            }
+        })
+    })
+}
 
-function multiply(x, y) {
-    return x * y;
-};
+input()
 
-function divide(x, y) {
-    return x / y;
-};
-
-function operate(operator, x, y) {
+function operate(numbers, operator) {
     if (operator == '+') {
-        return add(x, y);
+        return add(numbers);
     } else if (operator == '-') {
-        return subtract(x, y);
+        return subtract(numbers);
     } else if (operator == '*') {
-        return multiply(x, y);
+        return multiply(numbers);
     } else {
-        return divide(x, y);
+        return divide(numbers);
     }
 }
 
-console.log(operate('+', 5, 6))
+function add(numbers) {
+    const total = numbers.reduce((total, part) => {
+        return +total + +part
+      }, 0);
+      return total
+};
+
+function subtract(numbers) {
+    const total = numbers.reduce((total, part) => {
+        return +total - +part
+      }, 0);
+      return total
+};
+
+function multiply(numbers) {
+    const total = numbers.reduce((total, part) => {
+        return +total * +part
+      }, );
+      return total
+};
+
+function divide(numbers) {
+    const total = numbers.reduce((total, part) => {
+        return +total / +part
+      }, );
+      return total
+};
